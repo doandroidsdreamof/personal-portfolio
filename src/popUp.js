540fe3d1d06ce755bf8406d8ab7dd207321120bc
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-09-05 17:30:36 +0300

    circle open

--- src/popUp.js
+++ src/popUp.js
@@ -1,3 +1,4 @@
+
 // Dom //
 const popUpButton = document.querySelector('.popup__button');
 const closeButton = document.querySelector('.popup__module__line');
@@ -8,12 +9,14 @@ const languageButtons = document.querySelector('.popup__languagebuttons');
 const popUpHead = document.querySelector('.popup__head');
 const englishButton = document.querySelector('.popup__languagebuttons__english')
 const turkishButton = document.querySelector('.popup__languagebuttons__türkçe')
+const expand = document.querySelector('.popup__button__expand')
 
 // Colors //
-
+//clip-path: circle(50% at 50% 50%);
 const buttonColor = 'rgb(33, 150, 243)';
 const popUpTopColor = 'rgb(23, 23, 23)';
 const popUpButtonColor = 'white';
+
 
 // Open popup //
 popUpButton.addEventListener('click',(e) =>{
@@ -26,13 +29,15 @@ languageButtons.style.display = "";
 popUpHead.style.display = "block";
 turkishButton.style.visibility = "visible";
 englishButton.style.visibility = "visible";
+popUpButton.style.animation = "circle .3s ease-in normal";
+
 
 })
 
 
+
 // Close popup //
 closeButton.addEventListener('click',(e) =>{
-const target = e.target;
 e.stopPropagation();
 popUpButton.classList.remove('expand');
 popUpText.style.display = "";
@@ -43,8 +48,7 @@ languageButtons.style.display = "none";
 popUpHead.style.display = "none";
 turkishButton.style.visibility = "hidden";
 englishButton.style.visibility = "hidden";
-
-
+popUpButton.style.animation = "";
 })
 
 
