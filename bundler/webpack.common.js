aace07471c7c40b99e70d5bde3690063cad54e3b
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-09-07 00:34:09 +0300

    optimized pdf size

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -80,15 +80,6 @@ module.exports = {
             },
             
 
-            // Shaders
-            {
-                test: /\.(glsl|vs|fs|vert|frag)$/,
-                exclude: /node_modules/,
-                use: [
-                    'raw-loader',
-                    'glslify-loader'
-                ]
-            }
         ]
     }
 }
